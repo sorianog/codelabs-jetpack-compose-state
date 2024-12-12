3a366dbe2f911854cc2abe98870e657a0c5531c4
sorianog <13261711+sorianog@users.noreply.github.com>
2024-12-12 11:47:28 -0800

    Complete Step 8: Use rememberSaveable

--- app/src/main/java/com/sorianog/basicstatecodelab/ui/WaterCounter.kt
+++ app/src/main/java/com/sorianog/basicstatecodelab/ui/WaterCounter.kt
@@ -1,27 +1,23 @@
 package com.sorianog.basicstatecodelab.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.sorianog.basicstatecodelab.R
-import org.w3c.dom.Text
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableIntStateOf(0) }
+        var count by rememberSaveable { mutableIntStateOf(0) }
         if (count > 0) {
             Text(
                 text = stringResource(R.string.glasses_count_msg, count)
