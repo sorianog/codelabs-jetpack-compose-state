eddac8b276d00a5ff21865c33a0a136e9cb0140c
sorianog <13261711+sorianog@users.noreply.github.com>
2024-12-12 11:45:13 -0800

    Complete Step 7: Add task item

--- app/src/main/java/com/sorianog/basicstatecodelab/MainActivity.kt
+++ app/src/main/java/com/sorianog/basicstatecodelab/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,7 +22,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicStateCodelabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    containerColor = colorResource(android.R.color.holo_green_dark)
+                ) { innerPadding ->
                     WellnessScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
--- app/src/main/java/com/sorianog/basicstatecodelab/ui/WaterCounter.kt
+++ app/src/main/java/com/sorianog/basicstatecodelab/ui/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.sorianog.basicstatecodelab.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -8,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -20,7 +22,6 @@ import org.w3c.dom.Text
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableIntStateOf(0) }
-
         if (count > 0) {
             Text(
                 text = stringResource(R.string.glasses_count_msg, count)
